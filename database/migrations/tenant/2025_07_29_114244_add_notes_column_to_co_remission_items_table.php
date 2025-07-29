a255c6c0b4a7993abeb1949c70d26eb2dd55ed8f
Grafico Digital <erpnuvo@gmail.com>
2025-07-29 12:06:07 -0500

    agregar columnas de notas a quatations_items y co_remision_items

---
+++ database/migrations/tenant/2025_07_29_114244_add_notes_column_to_co_remission_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNotesColumnToCoRemissionItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('co_remission_items', function (Blueprint $table) {
+            $table->text('notes')->nullable()->after('tax'); // para que aparezca antes de la columna
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('co_remission_items', function (Blueprint $table) {
+            $table->dropColumn('notes');
+        });
+    }
+}
